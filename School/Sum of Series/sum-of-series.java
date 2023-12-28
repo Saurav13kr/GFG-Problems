d3b2d14bd2ce7bce385cc51c91728ced950dd9d4
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2023-12-28 17:39:32 +0530

    Added Solution - GfG to GitHub

---
+++ School/Sum of Series/sum-of-series.java
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+
+            long ans = new Solution().seriesSum(n);
+
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    long seriesSum(int n) {
+        return(long)n*(n+1)/2;
+    }
+}
