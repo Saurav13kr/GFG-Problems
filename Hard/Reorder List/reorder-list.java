3368b03e924c9086f1dc11f6aa0747851c2197aa
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-01-13 06:45:57 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Reorder List/reorder-list.java
@@ -0,0 +1,125 @@
+//{ Driver Code Starts
+import java.util.*;
+
+import java.io.*;
+
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+  public class ReorderList {
+    Node head; // head of lisl
+    Node last; // last of linked list
+
+    /* Linked list Node*/
+
+    /* Utility functions */
+
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node) {
+
+        if (head == null) {
+            head = node;
+            last = head;
+        } else {
+            // Node temp = head;
+            // while (temp.next != null) temp = temp.next;
+
+            last.next = node;
+            last = last.next;
+        }
+    }
+    /* Function to print linked list */
+    void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Drier program to test above functions */
+    public static void main(String args[])throws IOException {
+
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            int n = Integer.parseInt(in.readLine());
+            ReorderList llist = new ReorderList();
+
+            String s[]=in.readLine().trim().split(" ");
+            int a1 = Integer.parseInt(s[0]);
+            Node head = new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) {
+                int a = Integer.parseInt(s[i]);
+                llist.addToTheLast(new Node(a));
+            }
+
+            new Solution().reorderlist(llist.head);
+
+            llist.printList();
+
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* Following is the Linked list node structure */
+
+/*class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    void reorderlist(Node head) {
+        // Your code here
+        if (head == null || head.next == null || head.next.next == null) return;
+        ArrayList<Node> list = new ArrayList<>();
+        Node temp = head;
+
+        while (temp != null) {
+            list.add(temp);
+            temp = temp.next;
+        }
+
+        int length = list.size();
+        int i = 0;
+        int j = length - 1;
+
+        Node a = list.get(i);
+        Node b = list.get(j);
+
+        while (i <= j) {
+            b = list.get(j);
+            a.next = b;
+            i++;
+            a = list.get(i);
+            b.next = a;
+            j--;
+        }
+        
+        if (i == j) { // Handle case where there's an odd number of nodes
+            b.next = null;
+        } else {
+            b.next = null;
+        }
+    
+    }
+}
