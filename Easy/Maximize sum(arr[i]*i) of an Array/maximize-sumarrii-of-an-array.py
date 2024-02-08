dca17116b01a3637ef5c6f109b15bc3afdf81d57
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-02-08 11:13:04 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximize sum(arr[i]*i) of an Array/maximize-sumarrii-of-an-array.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    def Maximize(self, a, n): 
+        k=[]
+        a.sort()
+        # Complete the function
+        for i in range(len(a)):
+            k.append(a[i]*i) 
+        l=sum(k)
+        l=l%(10**9 +7)
+        return l
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+for _ in range(0,int(input())):
+    n = int(input())
+    arr = list(map(int,input().strip().split()))
+    ob=Solution()
+    print(ob.Maximize(arr, n))
+    
+# } Driver Code Ends
