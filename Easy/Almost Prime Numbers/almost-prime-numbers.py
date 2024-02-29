7c6a5479942c6ecf969da0ac98aca3f0408f0eb8
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-02-29 10:28:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Almost Prime Numbers/almost-prime-numbers.py
@@ -0,0 +1,31 @@
+#Your task is to complete this function
+#Function should print the required output in one line
+#donot print new Line
+def printKAlmostPrimes(k, n):
+    #Code here
+    primes = [2, 3, 5, 7, 11, 13]
+    
+    number = 2
+    while n:
+        fcount, temp = 0, number
+        
+        for prime in primes:
+            while temp % prime == 0:
+                temp //= prime
+                fcount += 1
+        
+        if fcount + (temp != 1) == k:
+            print(number, end=' ')
+            n -= 1
+        
+        number += 1
+#{ 
+ # Driver Code Starts
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        k,n = list(map(int, input().strip().split()))
+        printKAlmostPrimes(k, n)
+        print('')
+# Contributed by: Harshit sidhwa
+# } Driver Code Ends
