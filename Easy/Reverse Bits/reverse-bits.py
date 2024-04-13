1e86529973584537f8304b710845e694e6424709
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-04-13 11:44:20 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse Bits/reverse-bits.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+
+class Solution:
+    def reversedBits(self, x):
+        # code here 
+        s=format(x, '032b')
+        s=s[::-1]
+        return int(s,2)
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        X=int(input())
+        
+        ob = Solution()
+        print(ob.reversedBits(X))
+# } Driver Code Ends
