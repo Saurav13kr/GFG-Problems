1be4ce4ac650ced76d2d499339382befda269e03
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-06-06 19:42:23 +0530

    Update postfix-to-prefix-conversion.java

--- Medium/Postfix to Prefix Conversion/postfix-to-prefix-conversion.java
+++ Medium/Postfix to Prefix Conversion/postfix-to-prefix-conversion.java
@@ -25,9 +25,11 @@ class GFG {
 class Solution {
     static String postToPre(String post_exp) {
         Stack <String> s= new Stack<>();
-        for(int i=0; i<post_exp.length(); i++){
+        for(int i=0; i<post_exp.length(); i++)
+        {
             char c= post_exp.charAt(i);
             if(Character.isLetter(c))s.push(String.valueOf(c));
+                
             else{
                 String v=s.pop();
                 s.push(c+s.pop()+v);
