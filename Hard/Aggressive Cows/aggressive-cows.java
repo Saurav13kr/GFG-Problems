27e807badc99456b63e7a9109475308b92f6530f
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-06-08 00:52:15 +0530

    Update aggressive-cows.java

--- Hard/Aggressive Cows/aggressive-cows.java
+++ Hard/Aggressive Cows/aggressive-cows.java
@@ -1,12 +1,10 @@
-//{ Driver Code Starts
-// Initial Template for Java
+
 
 import java.io.*;
 import java.util.*;
 
 class GFG {
-    public static void main(String[] args) throws IOException 
-    {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int t;
         t = sc.nextInt();
