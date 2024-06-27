21c3e2bb0c1b5b35777764466a77a86faade5f18
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-06-27 12:13:29 +0530

    Update find-last-digit-of-ab-for-large-numbers.py

--- Hard/Find Last Digit Of a^b for Large Numbers/find-last-digit-of-ab-for-large-numbers.py
+++ Hard/Find Last Digit Of a^b for Large Numbers/find-last-digit-of-ab-for-large-numbers.py
@@ -2,20 +2,16 @@
 
 class Solution:
     def getLastDigit(self, a, b):
-        # code here 
         if b=='0' or a=='1':
             return 1
         elif b=='1':
             return a
         b = int(b)%4
+        
         if b==0:
             b=4
         a=int(a[-1])
         return (a**b)%10
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
 
 if __name__ == '__main__': 
     t = int (input ())
