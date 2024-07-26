f56cc2e97b7fd40298665ee749082d487da64536
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-07-26 13:39:50 +0530

    Update largest-square-formed-in-a-matrix.cpp

--- Difficulty: Medium/Largest square formed in a matrix/largest-square-formed-in-a-matrix.cpp
+++ Difficulty: Medium/Largest square formed in a matrix/largest-square-formed-in-a-matrix.cpp
@@ -4,17 +4,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-// } Driver Code Ends
-// User function Template for C++
-
 class Solution {
   public:
     int maxSquare(int n, int m, vector<vector<int>> mat) {
         int res = 0;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if(mat[i][j]==1 && i-1>=0 && j-1>=0) {
+                if(mat[i][j]==1 && i-1>=0 && j-1>=0) 
+                {
                     mat[i][j] = min(mat[i][j-1], min(mat[i-1][j-1], mat[i-1][j])) + 1;
                 }
                 res = max(res,mat[i][j]);
@@ -35,6 +32,7 @@ int main() {
         int n, m;
         cin >> n >> m;
         vector<vector<int>> mat(n, vector<int>(m, 0));
+      
         for (int i = 0; i < n * m; i++)
             cin >> mat[i / m][i % m];
 
