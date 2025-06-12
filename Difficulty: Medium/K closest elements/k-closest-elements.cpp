0ab05dcde76149765163b7cde791d21a42201af1
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2025-06-12 11:08:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K closest elements/k-closest-elements.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    vector<int> printKClosest(vector<int> arr, int k, int x) {
+        vector<int> ans;
+        int j = (lower_bound(arr.begin(), arr.end(), x) - arr.begin());
+        int i = j-1;
+        if(arr[j] == x) j++;
+        while(k > 0 && i >= 0 && j < arr.size()){
+            if(x-arr[i] >= arr[j]-x)    ans.push_back(arr[j++]);
+            else    ans.push_back(arr[i--]);
+            k--;
+        }
+        while(k > 0 && i >= 0){
+            ans.push_back(arr[i--]);
+            k--;
+        }
+        while(k > 0 && j < arr.size()){
+            ans.push_back(arr[j++]);
+            k--;
+        }
+        return ans;
+    }
+};
