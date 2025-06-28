c509d8b11f25e3189ddc00b4ae2f1397e47f0a18
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2025-06-28 11:48:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Counting elements in two arrays/counting-elements-in-two-arrays.java
+++ Difficulty: Medium/Counting elements in two arrays/counting-elements-in-two-arrays.java
@@ -1,23 +1,30 @@
 class Solution {
     public static ArrayList<Integer> countLessEq(int a[], int b[]) {
+        ArrayList<Integer> ans = new ArrayList<>();
         Arrays.sort(b);
-        ArrayList<Integer> res=new ArrayList<>();
         
-        for(int i=0;i<a.length;i++){
-            int l=0, r=b.length-1;
-            
-            while(l<=r){
-                int mid=l+(r-l)/2;
-                if(b[mid]<=a[i]){
-                    l=mid+1;
-                }
-                else{
-                    r=mid-1;
-                }
-            }
-            res.add(r+1);
+        for (int i = 0; i < a.length; i++) {
+            int cnt = binarySearch(b, a[i]);
+            while (cnt < b.length && b[cnt] == a[i]) cnt++;
+            ans.add(cnt);
         }
         
-        return res;
+        return ans;
+    }
+    
+    private static int binarySearch(int[] arr, int k) {
+        int low = 0;
+        int high = arr.length - 1;
+        
+        while (low <= high) 
+        {
+            int mid = low + (high - low) / 2;
+            
+            if (arr[mid] == k) return mid;
+            else if (arr[mid] > k) high = mid - 1;
+            else low = mid + 1;
+        }
+        
+        return low;
     }
 }
